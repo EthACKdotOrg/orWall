569c9e7308a670027312735c09c93ad7629f1674
Cédric Jeanneret <tengu@ethack.org>
2014-07-20 23:07:02 +0200

    removed a rule, it is now added from the app startup

--- app/src/main/res/raw/userinit.sh
+++ app/src/main/res/raw/userinit.sh
@@ -21,7 +21,7 @@ $IPTABLES -A INPUT -j fw_INPUT
 
 # NAT to TOR: only for apps we really want
 
-$IPTABLES -t nat -A OUTPUT -m owner --uid-owner $ORBOT_UID -j RETURN
+# $IPTABLES -t nat -A OUTPUT -m owner --uid-owner $ORBOT_UID -j RETURN
 $IPTABLES -t nat -A OUTPUT ! -o lo -p udp -m udp --dport 53 -j REDIRECT --to-ports 5400
 $IPTABLES -t nat -A OUTPUT -d 10.0.0.0/8 -j RETURN
 $IPTABLES -t nat -A OUTPUT -d 172.16.0.0/12 -j RETURN
