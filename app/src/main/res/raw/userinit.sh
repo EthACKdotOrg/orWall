342e700d4558d480e1128a1a20cc14fadf6c9b47
Cédric Jeanneret <tengu@ethack.org>
2014-07-23 18:12:27 +0200

    updated init-script — just blocking, opening is done from the app

--- app/src/main/res/raw/userinit.sh
+++ app/src/main/res/raw/userinit.sh
@@ -7,10 +7,8 @@ $IPTABLES -P OUTPUT DROP
 $IPTABLES -P INPUT DROP
 $IPTABLES -P FORWARD ACCEPT
 
-# Well, some rules are added to INPUT in order to accept stuff? No, sorry. You can't!
-$IPTABLES -I INPUT 1 -j REJECT
+# Accept local connections
 $IPTABLES -I INPUT 1 -i lo -j ACCEPT
-$IPTABLES -I INPUT 1 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # Seems some systems just want to play with default Policy. Let block them!
 $IPTABLES -I OUTPUT 1 -j REJECT
