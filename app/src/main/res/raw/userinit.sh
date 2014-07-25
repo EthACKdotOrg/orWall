b2452f2818bcc6482876df303e68aa59a254d38f
Cédric Jeanneret <tengu@ethack.org>
2014-07-26 00:36:38 +0200

    added anti-leak rule

--- app/src/main/res/raw/userinit.sh
+++ app/src/main/res/raw/userinit.sh
@@ -12,3 +12,7 @@ $IPTABLES -I INPUT 1 -i lo -j ACCEPT
 
 # Seems some systems just want to play with default Policy. Let block them!
 $IPTABLES -I OUTPUT 1 -j REJECT
+
+# ref: https://lists.torproject.org/pipermail/tor-talk/2014-March/032503.html
+$IPTABLES -I OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
+$IPTABLES -I OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP
