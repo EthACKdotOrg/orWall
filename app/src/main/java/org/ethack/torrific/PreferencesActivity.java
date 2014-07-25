685338747b7891568254378e20b0844277b4abcc
Cédric Jeanneret <tengu@ethack.org>
2014-07-26 00:21:29 +0200

    we have now working settings :)

--- app/src/main/java/org/ethack/torrific/PreferencesActivity.java
+++ app/src/main/java/org/ethack/torrific/PreferencesActivity.java
@@ -1,12 +1,17 @@
 package org.ethack.torrific;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
+import org.ethack.torrific.iptables.InitializeIptables;
+import org.ethack.torrific.lib.NATLiteSource;
+
 import java.util.Arrays;
 import java.util.List;
+
 
 public class PreferencesActivity extends PreferenceActivity {
 
@@ -32,21 +37,73 @@ public class PreferencesActivity extends PreferenceActivity {
     }
 
     public static class NetworkPrefs extends PreferenceFragment {
+
+        private SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                InitializeIptables iptables = new InitializeIptables(new NATLiteSource(getActivity()));
+                if (sharedPreferences.getBoolean(s, true) && s.equals("enforce_init_script")) {
+                    iptables.installInitScript(getActivity());
+                }
+                if (sharedPreferences.getBoolean(s, true) && s.equals("deactivate_init_script") && !sharedPreferences.getBoolean("enforce_init_script", true)) {
+                    iptables.removeIniScript();
+                }
+            }
+        };
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
             PreferenceManager.setDefaultValues(getActivity(), R.xml.network_preference, true);
-            addPreferencesFromResource(R.xml.fragment_net_pref);
+            addPreferencesFromResource(R.xml.fragment_init_pref);
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
         }
     }
 
     public static class OtherPrefs extends PreferenceFragment {
+        private SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                InitializeIptables iptables = new InitializeIptables(new NATLiteSource(getActivity()));
+                if (s.equals("enable_lan")) {
+                    iptables.LANPolicy(sharedPreferences.getBoolean(s, false));
+                }
+
+                if (s.equals("enable_tethering")) {
+                    iptables.enableTethering(sharedPreferences.getBoolean(s, false));
+                }
+                if (s.equals("enable_captive_portal")) {
+                    iptables.enableCaptiveDetection(sharedPreferences.getBoolean(s, false), getActivity());
+                }
+            }
+        };
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             PreferenceManager.setDefaultValues(getActivity(), R.xml.other_preferences, true);
-            addPreferencesFromResource(R.xml.fragment_other_prefs);
+            addPreferencesFromResource(R.xml.fragment_network_prefs);
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
         }
     }
+
 }
