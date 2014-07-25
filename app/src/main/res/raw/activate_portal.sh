41e4830f751dc9ca046b5cd64605583eacd00039
Cédric Jeanneret <tengu@ethack.org>
2014-07-26 00:18:14 +0200

    new script: manage portal detection (at least on 4.4.x)

---
+++ app/src/main/res/raw/activate_portal.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+settings put global captive_portal_server clients3.google.com
+settings put global captive_portal_detection_enabled 1
---
+++ app/src/main/res/raw/deactivate_portal.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+settings put global captive_portal_server 127.0.0.1
+settings put global captive_portal_detection_enabled 0
