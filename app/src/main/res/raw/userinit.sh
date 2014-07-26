6a4e831e1727b7551b359a3c52657e85b6f0334c
Cédric Jeanneret <tengu@ethack.org>
2014-07-26 11:04:10 +0200

    lock IPv6

--- app/src/main/res/raw/userinit.sh
+++ app/src/main/res/raw/userinit.sh
@@ -2,16 +2,19 @@
 IP6TABLES=/system/bin/ip6tables
 IPTABLES=/system/bin/iptables
 
-
 $IPTABLES -P OUTPUT DROP
+$IP6TABLES -P OUTPUT DROP
 $IPTABLES -P INPUT DROP
+$IP6TABLES -P INPUT DROP
 $IPTABLES -P FORWARD ACCEPT
 
 # Accept local connections
-$IPTABLES -I INPUT 1 -i lo -j ACCEPT
+$IPTABLES -I INPUT -i lo -j ACCEPT
+$IP6TABLES -I INPUT -i lo -j ACCEPT
 
 # Seems some systems just want to play with default Policy. Let block them!
-$IPTABLES -I OUTPUT 1 -j REJECT
+$IPTABLES -I OUTPUT -j REJECT
+$IP6TABLES -I OUTPUT -j REJECT
 
 # ref: https://lists.torproject.org/pipermail/tor-talk/2014-March/032503.html
 $IPTABLES -I OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
