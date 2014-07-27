3960be0714122e435ca4d7ec877db255e3b12513
Cédric Jeanneret <tengu@ethack.org>
2014-07-27 13:35:15 +0200

    added missing rules for browser bypass

--- app/src/main/java/org/ethack/torrific/iptables/InitializeIptables.java
+++ app/src/main/java/org/ethack/torrific/iptables/InitializeIptables.java
@@ -131,12 +131,15 @@ public class InitializeIptables {
         String[] rules = {
                 "-%c INPUT -m owner --uid-owner %d -m conntrack --ctstate RELATED,ESTABLISHED -p udp --sport 53 -j ACCEPT",
                 "-%c INPUT -m conntrack --ctstate ESTABLISHED -m owner --uid-owner %d -j ACCEPT",
+                "-%c OUTPUT -m owner --uid-owner %d -j ACCEPT",
+                "-%c OUTPUT -m owner --uid-owner %d -m conntrack --ctstate ESTABLISHED -j ACCEPT",
                 "-t nat -%c OUTPUT -m owner --uid-owner %d -j RETURN",
         };
         char action;
         if (status) { action = 'I'; } else { action = 'D'; }
 
         for (String rule: rules) {
+            Log.d("ManageCaptiveBrowser", String.format(rule, action, uid));
             iptRules.genericRule(String.format(rule, action, uid));
         }
     }
