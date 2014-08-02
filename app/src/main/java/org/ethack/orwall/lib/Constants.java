ef7b4f7c826e0f7dc84124700cc39a56cfef9f98
Cédric Jeanneret <tengu@ethack.org>
2014-08-02 15:07:26 +0200

    added missing constants

--- app/src/main/java/org/ethack/orwall/lib/Constants.java
+++ app/src/main/java/org/ethack/orwall/lib/Constants.java
@@ -27,9 +27,12 @@ public class Constants {
     public static final String ACTION_PORTAL = "org.ethack.orwall.backgroundProcess.action.portal";
     public static final String PARAM_ACTIVATE = "org.ethack.orwall.captive.activate";
 
-    public static final String ACTION_ADD_RULE = "org.ethack.orwall.backgroundProcess.action.add_rule";
-    public static final String PARAM_APPUID = "";
-    public static final String PARAM_APPNAME = "";
+    public static final String ACTION_ADD_RULE = "org.ethack.orwall.backgroundProcess.action.addRule";
+    public static final String PARAM_APPUID = "org.ethack.orwall.backgroundProcess.action.addRule.appUid";
+    public static final String PARAM_APPNAME = "org.ethack.orwall.backgroundProcess.action.addRule.appName";
+
+    public static final String ACTION_TETHER = "org.ethack.orwall.backgroundProcess.action.tethering";
+    public static final String PARAM_TETHER_STATUS = "org.ethack.orwall.backgroundProcess.action.tethering.status";
 
     public static final long ORBOT_SOCKS_PROXY = 9050;
     public static final long ORBOT_TRANSPROXY = 9040;
