257e1d07cc38be759ebbff3ef3737ad76c074b12
Cédric Jeanneret <tengu@ethack.org>
2014-08-24 18:19:44 +0200

    forgot to push this

--- app/src/main/java/org/ethack/orwall/lib/Constants.java
+++ app/src/main/java/org/ethack/orwall/lib/Constants.java
@@ -46,4 +46,5 @@ public class Constants {
     public final static String DB_ONION_TYPE_TOR = "Tor";
 
     public final static String I2P_APP_NAME = "net.i2p.android.router";
+    public final static String ORBOT_APP_NAME = "org.torproject.android";
 }
