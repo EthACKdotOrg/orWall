8224e4c0a592a9da42460cb8a202d59e2ad9f26d
Cédric Jeanneret <tengu@ethack.org>
2014-08-29 19:07:49 +0200

    some refactoring

--- app/src/main/java/org/ethack/orwall/BackgroundProcess.java
+++ app/src/main/java/org/ethack/orwall/BackgroundProcess.java
@@ -1,12 +1,16 @@
 package org.ethack.orwall;
 
+import android.app.AlertDialog;
 import android.app.IntentService;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 
 import org.ethack.orwall.iptables.InitializeIptables;
 import org.ethack.orwall.iptables.IptRules;
 import org.ethack.orwall.lib.Constants;
+import org.sufficientlysecure.rootcommands.RootCommands;
 
 /**
  * Created by cedric on 7/31/14.
@@ -14,14 +18,19 @@ import org.ethack.orwall.lib.Constants;
 public class BackgroundProcess extends IntentService {
 
     private boolean supportComment;
+    private InitializeIptables initializeIptables;
+    private IptRules iptRules;
 
     public BackgroundProcess() {
         super("BackgroundProcess");
-        this.supportComment = this.getApplicationContext().getSharedPreferences(Constants.PREFERENCES, MODE_PRIVATE).getBoolean(Constants.CONFIG_IPT_SUPPORTS_COMMENTS, false);
     }
 
     @Override
     protected void onHandleIntent(Intent workIntent) {
+        this.supportComment = this.getSharedPreferences(Constants.PREFERENCES, MODE_PRIVATE).getBoolean(Constants.CONFIG_IPT_SUPPORTS_COMMENTS, false);
+        this.initializeIptables = new InitializeIptables(this);
+        this.iptRules = new IptRules(this.supportComment);
+
         String action = workIntent.getStringExtra(Constants.ACTION);
         if (action.equals(Constants.ACTION_PORTAL)) {
             boolean activate = workIntent.getBooleanExtra(Constants.PARAM_ACTIVATE, false);
@@ -41,22 +50,18 @@ public class BackgroundProcess extends IntentService {
     }
 
     private void managePortal(boolean activate) {
-        InitializeIptables initializeIptables = new InitializeIptables(this);
-        initializeIptables.enableCaptiveDetection(activate, this);
+        this.initializeIptables.enableCaptiveDetection(activate, this);
     }
 
     private void addRule(Long appUID, String appName) {
-        IptRules iptRules = new IptRules(this.supportComment);
-        iptRules.natApp(this, appUID, 'A', appName);
+        this.iptRules.natApp(this, appUID, 'A', appName);
     }
 
     private void rmRule(Long appUID, String appName) {
-        IptRules iptRules = new IptRules(this.supportComment);
-        iptRules.natApp(this, appUID, 'D', appName);
+        this.iptRules.natApp(this, appUID, 'D', appName);
     }
 
     private void manageTether(boolean activate) {
-        InitializeIptables initializeIptables = new InitializeIptables(this);
-        initializeIptables.enableTethering(activate);
+        this.initializeIptables.enableTethering(activate);
     }
 }
