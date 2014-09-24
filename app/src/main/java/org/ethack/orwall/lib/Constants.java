2bd38e85dcabced22ace157850e0307c220f5db1
Cédric Jeanneret <tengu@ethack.org>
2014-09-24 18:26:08 +0200

    new constants

--- app/src/main/java/org/ethack/orwall/lib/Constants.java
+++ app/src/main/java/org/ethack/orwall/lib/Constants.java
@@ -23,6 +23,8 @@ public class Constants {
     public final static String PREF_KEY_POLIPO_ENABLED = "enable_polipo";
     public final static String PREF_KEY_BROWSER_GRACETIME = "browser_gracetime";
     public final static String CONFIG_IPT_SUPPORTS_COMMENTS = "ipt_comments";
+    public final static String PREF_KEY_ORWALL_ENABLED = "orwall_enabled";
+    public final static String PREF_KEY_LAN_ENABLED = "lan_enabled";
 
     public final static String IPTABLES = "/system/bin/iptables";
 
