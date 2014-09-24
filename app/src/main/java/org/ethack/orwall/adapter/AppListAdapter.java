52c08be07a96781ae6f169c7073aa386fcb6c440
Cédric Jeanneret <tengu@ethack.org>
2014-09-24 12:40:28 +0200

    Application fragment
    Now it shows up two separated lists, one for enabled application (i.e.
    the one we want to make through some Proxy), and disabled.
    
    Missing: actions.
    We have to do some more dev in order to get the following capabilities:
    - user touch an app, it should go to the right listing, and iptables
      rules should be updated
    - for enabled app, we have to add a new visual in order to let the user
      access advanced features
    
    Also, for "home" tab, we must update display in order to use the
    application settings.

--- app/src/main/java/org/ethack/orwall/adapter/AppListAdapter.java
+++ app/src/main/java/org/ethack/orwall/adapter/AppListAdapter.java
@@ -1,15 +1,25 @@
 package org.ethack.orwall.adapter;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import org.ethack.orwall.R;
 import org.ethack.orwall.lib.AppRule;
+import org.sufficientlysecure.rootcommands.util.Log;
+
 
 import java.util.List;
+
 
 /**
  * New adapter class
@@ -39,27 +49,26 @@ import java.util.List;
 public class AppListAdapter extends ArrayAdapter {
 
     private final static String TAG = "AppListAdapter";
-
     private final Context context;
-    private final List<AppRule> enabledPkgs;
-    private final List<PackageInfo> disabledPkgs;
+    private final Object[] apps;
 
     /**
      * Constructor.
+     *
      * @param context - application context
      * @param pkgs - list of all installed packages
-     * @param enabledPkgs - list of enabled packages (the ones having some rule associated with)
      */
-    public AppListAdapter(Context context, List<PackageInfo> pkgs, List<AppRule> enabledPkgs) {
-        super(context, R.layout.rowlayout, pkgs);
-
+    public AppListAdapter(Context context, List<AppRule> pkgs) {
+        super(context, R.layout.rowlayout, context.getPackageManager().getInstalledApplications(PackageManager.GET_PERMISSIONS));
         this.context = context;
-        this.enabledPkgs = enabledPkgs;
-        this.disabledPkgs = pkgs;
+        this.apps = pkgs.toArray();
+
+        Log.d(TAG, "Pkgs: "+ this.apps.length);
     }
 
     /**
      * Creates the view with both lists.
+     *
      * @param position - position in list
      * @param convertView - conversion view
      * @param parent - parent view group
@@ -67,6 +76,57 @@ public class AppListAdapter extends ArrayAdapter {
      */
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        return null;
+
+        LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.app_row, parent, false);
+            holder = new ViewHolder();
+            holder.textView = (TextView) convertView.findViewById(R.id.id_application);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        AppRule appRule = null;
+        try {
+            appRule = (AppRule) this.apps[position];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //Log.e(TAG, "Out of bound: "+ String.valueOf(position));
+            //Log.e(TAG, "Array size: "+ String.valueOf(this.apps.length));
+        }
+
+        if (appRule != null) {
+
+            PackageManager packageManager = this.context.getPackageManager();
+            ApplicationInfo applicationInfo = null;
+            try {
+                applicationInfo = packageManager.getApplicationInfo(appRule.getAppName(), PackageManager.GET_PERMISSIONS);
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e(TAG, "Application not found: " + appRule.getAppName());
+            }
+
+            if (applicationInfo != null) {
+
+                Drawable appIcon = packageManager.getApplicationIcon(applicationInfo);
+                appIcon.setBounds(0, 0, 40, 40);
+
+                holder.textView.setCompoundDrawables(appIcon, null, null, null);
+                if (appRule.getOnionType().equals("None")) {
+                    holder.textView.setText(packageManager.getApplicationLabel(applicationInfo));
+                    holder.textView.setTextColor(Color.GRAY);
+                } else {
+                    holder.textView.setTextColor(Color.WHITE);
+                    holder.textView.setText(appRule.getAppName() + "(" + appRule.getOnionType() + ")");
+                }
+            }
+        }
+        return convertView;
+    }
+
+    static class ViewHolder {
+        private TextView textView;
     }
 }
--- app/src/main/java/org/ethack/orwall/fragments/AppFragment.java
+++ app/src/main/java/org/ethack/orwall/fragments/AppFragment.java
@@ -1,14 +1,25 @@
 package org.ethack.orwall.fragments;
 
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import org.ethack.orwall.R;
+import org.ethack.orwall.adapter.AppListAdapter;
+import org.ethack.orwall.lib.AppRule;
+import org.ethack.orwall.lib.Constants;
+import org.ethack.orwall.lib.NatRules;
+import org.ethack.orwall.lib.PackageComparator;
+import org.sufficientlysecure.rootcommands.util.Log;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,12 +27,86 @@ import java.util.List;
  * @link org.ethack.orwall.TabbedMain
  */
 public class AppFragment extends Fragment {
+
+    private final static  String TAG = "AppFragment";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tabbed_apps, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_tabbed_apps, container, false);
+
+        ListView enabledListView = (ListView)view.findViewById(R.id.id_enabled_apps);
+        ListView disabledListView = (ListView)view.findViewById(R.id.id_disabled_apps);
+
+        NatRules natRules = new NatRules(this.getActivity());
+        List<AppRule> enabledApps = natRules.getAllRules();
+        List<AppRule> disabledApps = listDisabledApps();
+
+        Log.d(TAG, "Enabled size: " + String.valueOf(enabledApps.size()));
+        Log.d(TAG, "Disabled size: " + String.valueOf(disabledApps.size()));
+
+        if (natRules.getRuleCount() > 0) {
+            enabledListView.setAdapter(new AppListAdapter(this.getActivity(), enabledApps));
+        }
+        disabledListView.setAdapter(new AppListAdapter(this.getActivity(), disabledApps));
+        return view;
     }
 
-    private List<ApplicationInfo> listAllApps() {
-        return null;
+    private List<AppRule> listDisabledApps() {
+        NatRules natRules = new NatRules(this.getActivity());
+
+        PackageManager packageManager = this.getActivity().getPackageManager();
+        List<AppRule> pkgList = new ArrayList<AppRule>();
+
+        List<PackageInfo> pkgInstalled = packageManager.getInstalledPackages(PackageManager.GET_PERMISSIONS);
+        Collections.sort(pkgInstalled, new PackageComparator(packageManager));
+
+        for (PackageInfo pkgInfo: pkgInstalled) {
+            if (needInternet(pkgInfo) && !isReservedApp(pkgInfo)) {
+                if (!natRules.isAppInRules((long)pkgInfo.applicationInfo.uid)) {
+                    AppRule tmp = new AppRule(pkgInfo.packageName, (long)pkgInfo.applicationInfo.uid, "None", (long)0, "None");
+                    pkgList.add(tmp);
+                }
+            }
+        }
+        return pkgList;
+    }
+
+    /**
+     * Checks if package is System or not.
+     *
+     * @param pkgInfo
+     * @return true if package is a system app
+     */
+    private boolean isSystemPackage(PackageInfo pkgInfo) {
+        return ((pkgInfo.applicationInfo.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0);
+    }
+
+    /**
+     * Checks if application requires Internet
+     *
+     * @param pkg
+     * @return
+     */
+    private boolean needInternet(PackageInfo pkg) {
+        String[] permissions = (pkg.requestedPermissions);
+        if (permissions != null) {
+            for (String perm : permissions) {
+                if (perm.equals("android.permission.INTERNET")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if app name is a reserved one, like orbot or i2p
+     *
+     * @param pkg
+     * @return
+     */
+    private boolean isReservedApp(PackageInfo pkg) {
+        return (pkg.packageName.equals(Constants.ORBOT_APP_NAME) || pkg.packageName.equals(Constants.I2P_APP_NAME));
     }
 }
