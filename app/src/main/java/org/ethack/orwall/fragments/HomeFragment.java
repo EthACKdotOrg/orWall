92d63663de271af2fb8e7bb57b670bb5ed72e105
Cédric Jeanneret <tengu@ethack.org>
2014-09-24 19:31:08 +0200

    Allows to deactivate orWall from a single switch.
    This allows people to install orWall while having the possibility to
    deactivate it in some cases.
    Note: the deactivation will hold until the next reboot, we still have to
    do some stuff in initializeIptables.boot method regarding this.

--- app/src/main/java/org/ethack/orwall/fragments/HomeFragment.java
+++ app/src/main/java/org/ethack/orwall/fragments/HomeFragment.java
@@ -1,14 +1,22 @@
 package org.ethack.orwall.fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import org.ethack.orwall.BackgroundProcess;
 import org.ethack.orwall.R;
+import org.ethack.orwall.iptables.InitializeIptables;
 import org.ethack.orwall.lib.Constants;
 
 /**
@@ -16,12 +24,13 @@ import org.ethack.orwall.lib.Constants;
  * @link org.ethack.orwall.TabbedMain
  */
 public class HomeFragment extends Fragment {
+    private SharedPreferences sharedPreferences;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_tabbed_home, container, false);
 
-        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(Constants.PREFERENCES, this.getActivity().MODE_PRIVATE);
+        this.sharedPreferences = this.getActivity().getSharedPreferences(Constants.PREFERENCES, this.getActivity().MODE_PRIVATE);
 
         Switch orwallStatus = (Switch)view.findViewById(R.id.orwall_status);
         Switch browserStatus = (Switch)view.findViewById(R.id.browser_status);
@@ -29,10 +38,52 @@ public class HomeFragment extends Fragment {
         Switch lanStatus = (Switch)view.findViewById(R.id.lan_status);
 
         orwallStatus.setChecked(sharedPreferences.getBoolean(Constants.PREF_KEY_ORWALL_ENABLED, true));
+        orwallStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleOrwall(view);
+            }
+        });
         browserStatus.setChecked(sharedPreferences.getBoolean(Constants.PREF_KEY_BROWSER_ENABLED, false));
         sipStatus.setChecked(sharedPreferences.getBoolean(Constants.PREF_KEY_SIP_ENABLED, false));
         lanStatus.setChecked(sharedPreferences.getBoolean(Constants.PREF_KEY_LAN_ENABLED, false));
 
         return view;
+    }
+
+    public void toggleOrwall(final View view) {
+        boolean checked = ((Switch) view).isChecked();
+
+        final Intent bgpProcess = new Intent(this.getActivity(), BackgroundProcess.class);
+
+        if (checked) {
+            bgpProcess.putExtra(Constants.ACTION, Constants.ACTION_ENABLE_ORWALL);
+            getActivity().startService(bgpProcess);
+            sharedPreferences.edit().putBoolean(Constants.PREF_KEY_ORWALL_ENABLED, true).apply();
+            Toast.makeText(this.getActivity(), getString(R.string.enabling_orwall), Toast.LENGTH_LONG).show();
+
+        } else {
+            final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this.getActivity());
+            //final AlertDialog dialog = alertDialog.create();
+
+            alertDialog.setTitle(getString(R.string.disable_orwall_title));
+            alertDialog.setMessage(getString(R.string.disable_orwall_msg));
+            alertDialog.setPositiveButton(getString(R.string.alert_accept), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    bgpProcess.putExtra(Constants.ACTION, Constants.ACTION_DISABLE_ORWALL);
+                    getActivity().startService(bgpProcess);
+                    sharedPreferences.edit().putBoolean(Constants.PREF_KEY_ORWALL_ENABLED, false).apply();
+                }
+            });
+
+            alertDialog.setNegativeButton(getString(R.string.main_dismiss), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ((Switch) view).setChecked(true);
+                }
+            });
+            alertDialog.show();
+        }
     }
 }
