3864a78e30a629b35e93b81d125a83b2ba286fd2
Cédric Jeanneret <tengu@ethack.org>
2014-09-25 11:33:50 +0200

    Save advanced preferences
    This patch allows to save settings. Per application.
    It also calls a background process in order to ensure we do not lock the
    UI for nothing.

--- app/src/main/java/org/ethack/orwall/adapter/AppListAdapter.java
+++ app/src/main/java/org/ethack/orwall/adapter/AppListAdapter.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -18,6 +19,7 @@ import android.widget.RadioButton;
 
 import org.ethack.orwall.BackgroundProcess;
 import org.ethack.orwall.R;
+import org.ethack.orwall.iptables.IptRules;
 import org.ethack.orwall.lib.AppRule;
 import org.ethack.orwall.lib.Constants;
 import org.ethack.orwall.lib.NatRules;
@@ -34,18 +36,18 @@ import info.guardianproject.onionkit.ui.OrbotHelper;
  * It will provide, in the end, two lists:
  * - enabled application
  * - disabled application
- *
+ * <p/>
  * Enabled application will get the following information/characteristics:
  * - name of proxy being used (default: Tor/Orbot)
  * - if there's a pass-through, it will be shown in here
- *
+ * <p/>
  * A long press on the activated app will show up a dedicated dialog providing:
  * - choice for another proxy app (if i2p is installed for example)
  * - choice between "forcing" or "native" connection to the proxy
  * - native will create a fenced way: the app may not connect to anything else than the proxy port
  * - forcing will apply the -j REDIRECT we already use now
  * - choice to allow the app to go to the Net without using any proxy (with a timer)
- *
+ * <p/>
  * Once we touch either a disabled or enabled app, it should go to the opposite list, and rules should
  * be removed or added. This still has to be done.
  */
@@ -56,6 +58,7 @@ public class AppListAdapter extends ArrayAdapter {
     private final Object[] apps;
     private final PackageManager packageManager;
     private final NatRules natRules;
+    private final SharedPreferences sharedPreferences;
     private RadioButton radioTor;
     private RadioButton radioForced;
     private RadioButton radioFenced;
@@ -74,6 +77,7 @@ public class AppListAdapter extends ArrayAdapter {
         this.apps = pkgs.toArray();
         this.packageManager = context.getPackageManager();
         this.natRules = new NatRules(context);
+        this.sharedPreferences = context.getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
     }
 
     /**
@@ -102,7 +106,8 @@ public class AppListAdapter extends ArrayAdapter {
         AppRule appRule = null;
         try {
             appRule = (AppRule) this.apps[position];
-        } catch (ArrayIndexOutOfBoundsException e) {}
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
 
         if (appRule != null) {
 
@@ -206,7 +211,7 @@ public class AppListAdapter extends ArrayAdapter {
         View l_view = li.inflate(R.layout.advanced_connection, null);
 
         // Get application information
-        String appName = view.getTag(R.id.id_appTag).toString();
+        final String appName = view.getTag(R.id.id_appTag).toString();
         ApplicationInfo applicationInfo = null;
         try {
             applicationInfo = this.packageManager.getApplicationInfo(appName, PackageManager.GET_PERMISSIONS);
@@ -216,7 +221,7 @@ public class AppListAdapter extends ArrayAdapter {
         if (applicationInfo != null) {
 
             // get current application rule
-            AppRule appRule = this.natRules.getAppRule((long) applicationInfo.uid);
+            final AppRule appRule = this.natRules.getAppRule((long) applicationInfo.uid);
 
             // Add Proxy providers if available
             // Is orbot installed ?
@@ -251,8 +256,8 @@ public class AppListAdapter extends ArrayAdapter {
                 ((ViewGroup) l_view.findViewById(R.id.radio_connection_providers)).addView(radioI2p);
             }
 
-            this.radioForced = (RadioButton)l_view.findViewById(R.id.id_radio_force);
-            this.radioFenced = (RadioButton)l_view.findViewById(R.id.id_radio_fenced);
+            this.radioForced = (RadioButton) l_view.findViewById(R.id.id_radio_force);
+            this.radioFenced = (RadioButton) l_view.findViewById(R.id.id_radio_fenced);
             // Is it a Fenced or a Forced app?
             if (appRule.getPortType().equals(Constants.DB_PORT_TYPE_FENCED)) {
                 radioFenced.setChecked(true);
@@ -278,24 +283,25 @@ public class AppListAdapter extends ArrayAdapter {
             AlertDialog.Builder alert = new AlertDialog.Builder(context);
 
             // Will save state and apply rules
-            alert.setPositiveButton(R.string.alert_accept, new DialogInterface.OnClickListener() {
+            alert.setPositiveButton(R.string.alert_save, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-
+                    saveAdvanced(appRule);
                 }
             });
 
             // Will do nothing
-            alert.setNegativeButton(R.string.main_dismiss, new DialogInterface.OnClickListener() {
+            alert.setNegativeButton(R.string.alert_cancel, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
                 }
             });
 
             // Display alert
             alert.setTitle(String.format(
-                    this.context.getString(R.string.advanced_connection_settings_title),
-                    this.packageManager.getApplicationLabel(applicationInfo))
+                            this.context.getString(R.string.advanced_connection_settings_title),
+                            this.packageManager.getApplicationLabel(applicationInfo))
             ).setView(l_view).show();
         }
     }
@@ -306,5 +312,83 @@ public class AppListAdapter extends ArrayAdapter {
         radioI2p.setEnabled(!status);
         radioTor.setEnabled(!status);
         check_bypass.setChecked(status);
+    }
+
+    private void saveAdvanced(AppRule appRule) {
+
+        // Update DB content
+        AppRule updated = new AppRule();
+        updated.setAppName(appRule.getAppName());
+        updated.setAppUID(appRule.getAppUID());
+
+        if (check_bypass.isChecked()) {
+            updated.setOnionType(Constants.DB_ONION_TYPE_BYPASS);
+            updated.setPortType(Constants.DB_ONION_TYPE_BYPASS);
+        } else {
+            // Anonymity provider
+            if (radioTor.isChecked()) {
+                updated.setOnionType(Constants.DB_ONION_TYPE_TOR);
+            }
+            if (radioI2p.isChecked()) {
+                updated.setOnionType(Constants.DB_ONION_TYPE_I2P);
+            }
+            // We might fallback to the default type.
+            if (updated.getOnionType() == null || updated.getOnionType().isEmpty()) {
+                updated.setOnionType(Constants.DB_ONION_TYPE_TOR);
+            }
+        }
+        // Anonymity connectivity
+        if (radioFenced.isChecked()) {
+            updated.setPortType(Constants.DB_PORT_TYPE_FENCED);
+        }
+        if (radioForced.isChecked()) {
+            updated.setPortType(Constants.DB_PORT_TYPE_TRANS);
+        }
+
+        // We might fallback to the default type: forced
+        if (updated.getPortType() == null || updated.getPortType().isEmpty()) {
+            updated.setPortType(Constants.DB_PORT_TYPE_TRANS);
+        }
+
+        // If and ONLY IF the DB update is OK, we will push the new rules
+        if (natRules.update(updated)) {
+
+            // We want to use a background process in order to free the main thread and associated
+            Intent bgCleanup = new Intent(this.context, BackgroundProcess.class);
+            bgCleanup.putExtra(Constants.PARAM_APPUID, appRule.getAppUID());
+            bgCleanup.putExtra(Constants.PARAM_APPNAME, appRule.getAppName());
+
+            // first: clean existing rules
+            IptRules iptRules = new IptRules(this.sharedPreferences.getBoolean(Constants.CONFIG_IPT_SUPPORTS_COMMENTS, false));
+            if (appRule.getPortType().equals(Constants.DB_PORT_TYPE_FENCED)) {
+                bgCleanup.putExtra(Constants.ACTION, Constants.ACTION_RM_FENCED);
+
+            } else if (appRule.getOnionType().equals(Constants.DB_ONION_TYPE_BYPASS)) {
+                bgCleanup.putExtra(Constants.ACTION, Constants.ACTION_RM_BYPASS);
+
+            } else {
+                bgCleanup.putExtra(Constants.ACTION, Constants.ACTION_RM_RULE);
+            }
+            this.context.startService(bgCleanup);
+
+            // Now we can add the new rules
+            Intent bgNewRules = new Intent(this.context, BackgroundProcess.class);
+            bgNewRules.putExtra(Constants.PARAM_APPUID, appRule.getAppUID());
+            bgNewRules.putExtra(Constants.PARAM_APPNAME, appRule.getAppName());
+
+            if (updated.getPortType().equals(Constants.DB_PORT_TYPE_FENCED)) {
+                bgNewRules.putExtra(Constants.ACTION, Constants.ACTION_ADD_FENCED);
+
+            } else if (updated.getOnionType().equals(Constants.DB_ONION_TYPE_BYPASS)) {
+                bgNewRules.putExtra(Constants.ACTION, Constants.ACTION_ADD_BYPASS);
+
+            } else {
+                bgNewRules.putExtra(Constants.ACTION, Constants.ACTION_ADD_RULE);
+            }
+            this.context.startService(bgNewRules);
+        } else {
+            Log.e(TAG, "ERROR while updating object in DB!");
+        }
+
     }
 }
