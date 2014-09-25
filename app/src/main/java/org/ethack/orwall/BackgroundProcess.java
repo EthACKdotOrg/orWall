3208e02729104b2f139640a44575d3001e3b0439
Cédric Jeanneret <tengu@ethack.org>
2014-09-25 12:20:14 +0200

    added some spaces in order to make code more readable

--- app/src/main/java/org/ethack/orwall/BackgroundProcess.java
+++ app/src/main/java/org/ethack/orwall/BackgroundProcess.java
@@ -29,21 +29,27 @@ public class BackgroundProcess extends IntentService {
         if (action.equals(Constants.ACTION_PORTAL)) {
             boolean activate = workIntent.getBooleanExtra(Constants.PARAM_ACTIVATE, false);
             managePortal(activate);
+
         } else if (action.equals(Constants.ACTION_ADD_RULE)) {
             long appUID = workIntent.getLongExtra(Constants.PARAM_APPUID, 0);
             String appName = workIntent.getStringExtra(Constants.PARAM_APPNAME);
             addRule(appUID, appName);
+
         } else if (action.equals(Constants.ACTION_RM_RULE)) {
             long appUID = workIntent.getLongExtra(Constants.PARAM_APPUID, 0);
             String appName = workIntent.getStringExtra(Constants.PARAM_APPNAME);
             rmRule(appUID, appName);
+
         } else if (action.equals(Constants.ACTION_TETHER)) {
             boolean activate = workIntent.getBooleanExtra(Constants.PARAM_TETHER_STATUS, false);
             manageTether(activate);
+
         } else if (action.equals(Constants.ACTION_DISABLE_ORWALL)) {
             this.initializeIptables.deactivate();
+
         } else if (action.equals(Constants.ACTION_ENABLE_ORWALL)) {
             this.initializeIptables.boot();
+
         } else if (action.equals(Constants.ACTION_RM_BYPASS) || action.equals(Constants.ACTION_ADD_BYPASS)) {
             String appName = workIntent.getStringExtra(Constants.PARAM_APPNAME);
             long appUID = workIntent.getLongExtra(Constants.PARAM_APPUID, 0);
