bd3e993dc45ca952be9dcb61c3745a970a2e6fa4
Cédric Jeanneret <tengu@ethack.org>
2014-09-26 11:17:50 +0200

    added some more information in the Wizard regarding orWall status

--- app/src/main/java/org/ethack/orwall/fragments/WizardFragment.java
+++ app/src/main/java/org/ethack/orwall/fragments/WizardFragment.java
@@ -2,16 +2,23 @@ package org.ethack.orwall.fragments;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.ethack.orwall.R;
+import org.ethack.orwall.iptables.InitializeIptables;
 import org.ethack.orwall.lib.Constants;
+import org.ethack.orwall.lib.InstallScripts;
+import org.sufficientlysecure.rootcommands.RootCommands;
+
+import info.guardianproject.onionkit.ui.OrbotHelper;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -82,13 +89,64 @@ public class WizardFragment extends Fragment {
         }
         ((TextView) rootView.findViewById(R.id.wizard_fragment_content)).setText(fragment);
 
-        ((Button) rootView.findViewById(R.id.wizard_close)).setOnClickListener(new View.OnClickListener() {
+        rootView.findViewById(R.id.wizard_close).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE).edit().putBoolean(Constants.PREF_KEY_FIRST_RUN, false).apply();
                 getActivity().finish();
             }
         });
+
+        // Add some stuff on the very first Wizard page
+        if (mPageNumber == 0) {
+            ViewGroup main_content = (ViewGroup) rootView.findViewById(R.id.id_main_content);
+            SharedPreferences preferenceManager = getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
+            InitializeIptables initializeIptables = new InitializeIptables(getActivity());
+
+            // Root status
+            Switch rootStatus = new Switch(getActivity());
+            rootStatus.setChecked(RootCommands.rootAccessGiven());
+            rootStatus.setEnabled(false);
+            rootStatus.setText(getString(R.string.wizard_init_root_text));
+            main_content.addView(rootStatus);
+
+            // Does iptables exist?
+            Switch iptablesStatus = new Switch(getActivity());
+            iptablesStatus.setChecked(initializeIptables.iptablesExists());
+            iptablesStatus.setEnabled(false);
+            iptablesStatus.setText(getString(R.string.wizard_init_iptables_text));
+            main_content.addView(iptablesStatus);
+
+            // Does current kernel support IPTables comments?
+            initializeIptables.supportComments();
+            Switch iptablesComments = new Switch(getActivity());
+            iptablesComments.setChecked(preferenceManager.getBoolean(Constants.CONFIG_IPT_SUPPORTS_COMMENTS, false));
+            iptablesComments.setEnabled(false);
+            iptablesComments.setText(getString(R.string.wizard_init_ipt_comments_text));
+            main_content.addView(iptablesComments);
+
+            // init-script installation
+            InstallScripts installScripts = new InstallScripts(getActivity());
+            installScripts.run();
+            boolean disableInit = preferenceManager.getBoolean(Constants.PREF_KEY_DISABLE_INIT, false);
+
+            Switch initScript = new Switch(getActivity());
+            initScript.setChecked(disableInit);
+            initScript.setText(getString(R.string.wizard_init_script_text));
+            initScript.setEnabled(disableInit);
+            // TODO: add listener
+            main_content.addView(initScript);
+
+            // Is orbot installed?
+            OrbotHelper orbotHelper = new OrbotHelper(getActivity());
+            Switch orbotStatus = new Switch(getActivity());
+            orbotStatus.setChecked(orbotHelper.isOrbotInstalled());
+            orbotStatus.setEnabled(false);
+            orbotStatus.setText(getString(R.string.wizard_orbot_status_text));
+            main_content.addView(orbotStatus);
+
+        }
+
         return rootView;
     }
 
