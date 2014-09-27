512d8780736d94d5151f72a46fd3410354222428
Cédric Jeanneret <tengu@ethack.org>
2014-09-27 16:38:45 +0200

    BootBroadcast must ensure we have the init-script installed
    Doing so will allow us to ensure, on each boot, the init-script is
    present.
    This will NOT correct issue #46. This one is a bit nastier, as it seems
    some ROMs have issue with the userinit.d location. We suspect crypted
    devices to cause some problems at this level (maybe the data directory
    isn't present early enough).

--- app/src/main/java/org/ethack/orwall/BootBroadcast.java
+++ app/src/main/java/org/ethack/orwall/BootBroadcast.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 
 import org.ethack.orwall.iptables.InitializeIptables;
+import org.ethack.orwall.lib.Constants;
 
+/**
+ * Do think at startup.
+ */
 public class BootBroadcast extends BroadcastReceiver {
 
     public BootBroadcast() {
@@ -15,7 +19,20 @@ public class BootBroadcast extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent intent) {
 
         InitializeIptables initializeIptables = new InitializeIptables(context);
+
+        // We want to ensure we support comments — reboot may be due to
+        // some ROM change, hence kernel may have changed as well.
         initializeIptables.supportComments();
+
+        // Enforce init-script if sharedpreference says it
+        // We want to do it the earlier.
+        // Also, we want to get a fresh status regarding the init-script support: this can be
+        // a reboot after a ROM upgrade or change.
+        boolean enforceInit = context.getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE).getBoolean(Constants.PREF_KEY_ENFOCE_INIT, true);
+        if (initializeIptables.initSupported() && enforceInit) {
+            initializeIptables.installInitScript();
+        }
+        // Apply boot-up rules in order to enable traffic for orbot and other things.
         initializeIptables.boot();
     }
 }
