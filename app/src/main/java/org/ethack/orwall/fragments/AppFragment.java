920862535c15f3b8c157ac26d966cec3be748f2c
Henri Gourvest <hgourvest@progdigy.com>
2016-07-07 21:04:13 +0200

    show app list even orwell not activated
    if application start unactivated it is not possible to show app list
    even if you enable option, you have to kill app.

--- app/src/main/java/org/ethack/orwall/fragments/AppFragment.java
+++ app/src/main/java/org/ethack/orwall/fragments/AppFragment.java
@@ -43,69 +43,64 @@ public class AppFragment extends Fragment {
         View view;
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
 
-        if (sharedPreferences.getBoolean(Constants.PREF_KEY_ORWALL_ENABLED, true)) {
-
-            view  = inflater.inflate(R.layout.fragment_tabbed_apps, container, false);
-            InitializeIptables initializeIptables = new InitializeIptables(getActivity());
-            // Do we have root access ?
-            if (RootCommands.rootAccessGiven()) {
-                view.findViewById(R.id.warn_root).setVisibility(View.GONE);
-            } else {
-                view.findViewById(R.id.warn_root).setVisibility(View.VISIBLE);
-            }
-            // Hopefully there IS iptables on this device…
-            if (initializeIptables.iptablesExists()) {
-                view.findViewById(R.id.warn_iptables).setVisibility(View.GONE);
-            } else {
-                view.findViewById(R.id.warn_iptables).setVisibility(View.VISIBLE);
-            }
-            if (initializeIptables.initSupported() && !initializeIptables.isInitialized()) {
-                view.findViewById(R.id.warn_init).setVisibility(View.VISIBLE);
-            }
-
-            ListView listView = (ListView) view.findViewById(R.id.id_enabled_apps);
-
-            // Toggle hint layer
-            boolean hide_hint = getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE)
-                    .getBoolean(Constants.PREF_KEY_HIDE_PRESS_HINT, false);
-
-            if (hide_hint) {
-                view.findViewById(R.id.hint_press).setVisibility(View.GONE);
-            } else {
-                view.findViewById(R.id.id_hide_hint).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        ((View) view.getParent()).setVisibility(View.GONE);
-                        getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE)
-                                .edit()
-                                .putBoolean(Constants.PREF_KEY_HIDE_PRESS_HINT, true).apply();
-                    }
-                });
-            }
-
-            // get enabled apps
-            NatRules natRules = new NatRules(this.getActivity());
-            List<AppRule> enabledApps = natRules.getAllRules();
-
-            // get disabled apps (filtered with enabled)
-            List<AppRule> disabledApps = listDisabledApps();
-            // Get special, disabled apps
-            List<AppRule> specialDisabled = listSpecialApps();
-
-            // Merge both disabled apps
-            disabledApps.addAll(specialDisabled);
-
-            // Sort collection using a dedicated method
-            Collections.sort(enabledApps, new AppRuleComparator(getActivity().getPackageManager()));
-            Collections.sort(disabledApps, new AppRuleComparator(getActivity().getPackageManager()));
-
-            // merge both collections so that enabled apps are above disabled
-            enabledApps.addAll(disabledApps);
-
-            listView.setAdapter(new AppListAdapter(this.getActivity(), enabledApps));
+        view  = inflater.inflate(R.layout.fragment_tabbed_apps, container, false);
+        InitializeIptables initializeIptables = new InitializeIptables(getActivity());
+        // Do we have root access ?
+        if (RootCommands.rootAccessGiven()) {
+            view.findViewById(R.id.warn_root).setVisibility(View.GONE);
         } else {
-            view  = inflater.inflate(R.layout.fragment_tabbed_noapps, container, false);
+            view.findViewById(R.id.warn_root).setVisibility(View.VISIBLE);
         }
+        // Hopefully there IS iptables on this device…
+        if (initializeIptables.iptablesExists()) {
+            view.findViewById(R.id.warn_iptables).setVisibility(View.GONE);
+        } else {
+            view.findViewById(R.id.warn_iptables).setVisibility(View.VISIBLE);
+        }
+        if (initializeIptables.initSupported() && !initializeIptables.isInitialized()) {
+            view.findViewById(R.id.warn_init).setVisibility(View.VISIBLE);
+        }
+
+        ListView listView = (ListView) view.findViewById(R.id.id_enabled_apps);
+
+        // Toggle hint layer
+        boolean hide_hint = getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE)
+                .getBoolean(Constants.PREF_KEY_HIDE_PRESS_HINT, false);
+
+        if (hide_hint) {
+            view.findViewById(R.id.hint_press).setVisibility(View.GONE);
+        } else {
+            view.findViewById(R.id.id_hide_hint).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ((View) view.getParent()).setVisibility(View.GONE);
+                    getActivity().getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE)
+                            .edit()
+                            .putBoolean(Constants.PREF_KEY_HIDE_PRESS_HINT, true).apply();
+                }
+            });
+        }
+
+        // get enabled apps
+        NatRules natRules = new NatRules(this.getActivity());
+        List<AppRule> enabledApps = natRules.getAllRules();
+
+        // get disabled apps (filtered with enabled)
+        List<AppRule> disabledApps = listDisabledApps();
+        // Get special, disabled apps
+        List<AppRule> specialDisabled = listSpecialApps();
+
+        // Merge both disabled apps
+        disabledApps.addAll(specialDisabled);
+
+        // Sort collection using a dedicated method
+        Collections.sort(enabledApps, new AppRuleComparator(getActivity().getPackageManager()));
+        Collections.sort(disabledApps, new AppRuleComparator(getActivity().getPackageManager()));
+
+        // merge both collections so that enabled apps are above disabled
+        enabledApps.addAll(disabledApps);
+
+        listView.setAdapter(new AppListAdapter(this.getActivity(), enabledApps));
 
         return view;
     }
